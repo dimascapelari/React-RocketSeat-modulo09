37032def1af0d6d2cd373b53bbb2fab982cd6650
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-28 11:42:37 -0300

    Aula 03 - Hook useEffect, Aula 04 - Hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
 function App() {
-  const [tech, setTech] = useState(["ReactJS", "React Native"]);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState("");
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech("");
   }
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem(tech);
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // Salvar os dados do localStorage
+  useEffect(() => {
+    localStorage.setItem("tech", JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
 
   return (
     <>
@@ -16,6 +31,8 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
       <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
